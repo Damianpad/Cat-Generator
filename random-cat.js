b191ef5ea1677351312d0a87de54cb6326653dda
Damian Padilla <damipad4@gmail.com>
2022-08-06 13:16:59 -0500

    Added chalk

---
+++ random-cat.js
@@ -0,0 +1,27 @@
+import chalk from 'chalk';
+
+
+// var randomCat = require('random-cat');
+
+// let url = randomCat.get();
+
+// console.log(url);
+
+// document.getElementById("randomCat").src = randomCat;
+
+// var randomImageGenerator = require('random-image-generator'),
+//     fs = require('fs');
+
+// /**
+//  * Takes a width and height parameter
+//  * Returns a Buffer containing a JPEG image
+//  */
+// randomImageGenerator(800, 600, function (err, image) {
+//     fs.writeFile('random-image.jpg', image, function (err) {
+//         // done, hopefully
+//     });
+// });
+
+// console.log(randomImageGenerator());
+
+console.log(chalk.blue('Hello world!'));
